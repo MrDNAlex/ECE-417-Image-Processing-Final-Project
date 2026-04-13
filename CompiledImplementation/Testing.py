5df4a9d0c13986c960470cd18a26995b6e53b2c6
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-04-12 23:59:06 -0400

    Cleanup

--- CompiledImplementation/Testing.py
+++ CompiledImplementation/Testing.py
@@ -6,17 +6,14 @@ alpha = 0.01
 threshold = 0.7
 width = 854
 height = 480
-
+horizontalVideo = True
 useMorphology = True
 morphSize = 3
-horizontalVideo = True
 
 if horizontalVideo:
     settings = VideoProcessorSettings(K, alpha, threshold, width, height, useMorphology, morphSize)
 else:
     settings = VideoProcessorSettings(K, alpha, threshold, height, width, useMorphology, morphSize)
 
-processor = VideoProcessor("Implementation/Data/Universal.mp4", settings)
-
+processor = VideoProcessor("Videos/Traffic.mp4", settings)
 processor.run()
-
